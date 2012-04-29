575a96bcf91b93a0c6bce139b777cf03cfb7d350
Derek Willis <dwillis@gmail.com>
2012-04-29 11:37:14 -0400

    admin css

--- tracker/settings.py
+++ tracker/settings.py
@@ -53,7 +53,7 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://www.forjournalists.com/media/'
+MEDIA_URL = ''
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -63,7 +63,7 @@ STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://www.forjournalists.com/media/'
 
 ADMIN_MEDIA_PREFIX = 'http://www.forjournalists.com/media/admin/'
 
