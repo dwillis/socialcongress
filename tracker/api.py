841a6e8a40b24e2f762bd49545dc515071643514
Derek Willis <dwillis@gmail.com>
2012-05-30 21:35:23 -0400

    removed member reports

--- tracker/api.py
+++ tracker/api.py
@@ -2,15 +2,6 @@ from tastypie.resources import ModelResource
 from tracker.models import Member, Report
 from tastypie import fields
 
-class MemberResource(ModelResource):
-    reports = fields.ToManyField('tracker.api.ReportResource', 'reports', full = True)
-    
-    class Meta:
-        queryset = Member.objects.all()
-        resource_name = 'member'
-        excludes = ['race', 'status_id', 'service', 'margin_2010', 'social_networks', 'facebook_10', 'facebook_status', 'twitter_10', 'twitter_status', 'index_10']
-        allowed_methods = ['get']
-
 class ReportResource(ModelResource):
     member = fields.ForeignKey('tracker.api.MemberResource', 'member')
     
@@ -18,3 +9,10 @@ class ReportResource(ModelResource):
         queryset = Report.objects.all()
         resource_name = 'report'
 
+class MemberResource(ModelResource):
+
+    class Meta:
+        queryset = Member.objects.all()
+        resource_name = 'member'
+        excludes = ['race', 'status_id', 'service', 'margin_2010', 'social_networks', 'facebook_10', 'facebook_status', 'twitter_10', 'twitter_status', 'index_10']
+        allowed_methods = ['get']
