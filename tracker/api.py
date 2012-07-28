c60cf8bdf11067d335ee20121b32d5860974d0f1
Derek Willis <dwillis@gmail.com>
2012-07-28 10:08:37 -0400

    added report resource to API

--- tracker/api.py
+++ tracker/api.py
@@ -1,18 +1,21 @@
 from tastypie.resources import ModelResource
 from tracker.models import Member, Report
 from tastypie import fields
-
-class ReportResource(ModelResource):
-    member = fields.ForeignKey('tracker.api.MemberResource', 'member')
-    
-    class Meta:
-        queryset = Report.objects.all()
-        resource_name = 'report'
+from tastypie.cache import SimpleCache
 
 class MemberResource(ModelResource):
-
+    
+    reports = fields.ToManyField('myapp.api.resources.ReportResource', 'reports')
+    
     class Meta:
         queryset = Member.objects.all()
         resource_name = 'member'
         excludes = ['race', 'status_id', 'service', 'margin_2010', 'social_networks', 'facebook_10', 'facebook_status', 'twitter_10', 'twitter_status', 'index_10']
         allowed_methods = ['get']
+        cache = SimpleCache(timeout=600)
+
+class ReportResource(ModelResource):
+    member = fields.ToOneField(MemberResource, 'members')
+
+    class Meta:
+        queryset = Report.objects.all()
